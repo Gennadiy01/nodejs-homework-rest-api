dfe68a230c86616926ed5f35ace5d3e036f40535
Gennadiy01 <gennadiy.d@ukr.net>
2023-01-05 14:45:33 +0200

    виконано в основному

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,135 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.resolve(__dirname, "./contacts.json");
+const { v4: uuidv4 } = require("uuid");
 
-const listContacts = async () => {}
+// ====   окрема функція зчитування файлів   =======================
+async function readDb() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const db = JSON.parse(data);
 
-const getContactById = async (contactId) => {}
+  return db;
+}
 
-const removeContact = async (contactId) => {}
+// =========== окрема функція запису в файл ====================================================================
 
-const addContact = async (body) => {}
+const writeDb = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const updateContact = async (contactId, body) => {}
+// ====   функція зчитування контактів   =======================
+async function listContacts() {
+  try {
+    const data = await readDb();
+    // console.table(data);
 
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ======== функція пошуку контактів ============================
+async function getContactById(contactId) {
+  try {
+    const contacts = await readDb();
+
+    const contact = contacts.find((contact) => contact.id === contactId);
+
+    // if (contact) {
+    //   console.log(`Contact with such id = ${contactId} was found!`.green);
+    //   console.table(contact);
+    // } else {
+    //   console.log(`Contact with such id = ${contactId} was not found!`.red);
+    // }
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ==========================  функція додавання контакту   =========================
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await readDb();
+    // ========= Перевірка полів контакту на повторюваність ===============v
+
+    const contactByName = contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    const contactByMail = contacts.find((contact) => contact.email === email);
+    const contactByPhone = contacts.find((contact) => contact.phone === phone);
+
+    if (contactByName) throw new Error("This name already exists!");
+
+    if (contactByMail) throw new Error("This email already exists!");
+
+    if (contactByPhone) throw new Error("This phone already exists!");
+
+    // ==================================================================^
+
+    const id = uuidv4();
+
+    const newContact = { id, name, email, phone };
+    contacts.push(newContact);
+
+    writeDb(contacts);
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ===========функція видалення контактів  ==========================
+async function removeContact(contactId) {
+  try {
+    const contacts = await readDb();
+
+    const idContact = contacts.findIndex((contact) => contact.id === contactId);
+    if (idContact === -1) {
+      // return console.log(`Contact with id=${contactId} not found!!!`.red);
+      return null;
+    }
+
+    const contactDelete = contacts.splice(idContact, 1);
+
+    writeDb(contacts);
+    // console.log(`Contact with id=${contactId} is removed`.green);
+    // console.table(contactDelete);
+
+    return contactDelete;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ==========================  функція оновлення контакту   =========================
+async function updateContact(contactId, body) {
+  try {
+    const contacts = await readDb();
+    const oldContact = await contacts.find(
+      (contact) => contact.id === contactId
+    );
+    if (oldContact) {
+      const updatedContact = { ...oldContact, ...body };
+      const index = contacts.indexOf(oldContact);
+      contacts.splice(index, 1, updatedContact);
+
+      writeDb(contacts);
+
+      return updatedContact;
+    }
+    return null;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ===================================================================================
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,89 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// ================ валідація =====================================V
+const schema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30),
+  email: Joi.string().email().min(3).max(30),
+  phone: Joi.string()
+    .min(7)
+    .max(30)
+    .pattern(/^\+|\d[0-9()]*\d$/, "numbers"),
+});
+// ================================================================^
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  return res.json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const contact = await getContactById(req.params.contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (!contact) {
+    return res.status(404).json({
+      message: `Contact with id=${req.params.contactId} is not found!`,
+    });
+  }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  return res.json(contact);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  // =============== валідація=================================V
 
-module.exports = router
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).json({ message: error.details[0].message });
+  // ==========================================================^
+
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    return res.status(400).json({ message: "Mssing required name field" });
+  }
+
+  const newContact = await addContact(name, email, phone);
+
+  return res.status(201).json(newContact);
+});
+
+/// ============================================================================
+router.delete("/:contactId", async (req, res, next) => {
+  const contact = await removeContact(req.params.contactId);
+
+  if (contact)
+    return res.json({
+      message: `Contact with id=${req.params.contactId} is deleted`,
+    });
+
+  res
+    .status(404)
+    .json({ message: `Contact with id=${req.params.contactId} not found!` });
+  // ============================================================================
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).json({ message: error.details[0].message });
+
+  const { name, email, phone } = req.body;
+
+  if (!name && !email && !phone)
+    return res.status(400).json({ message: "Missing fields" });
+  const response = await updateContact(req.params.contactId, req.body);
+
+  if (response) return res.json(response);
+  return res
+    .status(404)
+    .json({ message: `Contact with id=${req.params.contactId} not found!` });
+});
+
+module.exports = router;
